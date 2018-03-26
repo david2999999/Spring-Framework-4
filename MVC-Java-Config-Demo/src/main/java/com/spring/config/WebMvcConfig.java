5106729fc2bde505a7603a7075319894884fca0f
David Jiang <djiang86@binghamton.edu>
2018-03-26 22:50:38 +0000

    pure java configuration mvc

---
+++ MVC-Java-Config-Demo/src/main/java/com/spring/config/WebMvcConfig.java
@@ -0,0 +1,13 @@
+package com.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan("com.spring")
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+}
