7bbf4f6fdcc967d10095cd50344ae6642ed897b7
David Jiang <djiang86@binghamton.edu>
2018-03-27 20:58:00 +0000

    JSTL sql and c tags to display data from database

---
+++ MVC-JNDI-Demo/WebContent/WEB-INF/views/listOrganization2.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+ <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>List Organization Using a Service</title>
+</head>
+<body>
+	<h1>Listing Organizations Using a Service</h1>
+	<c:forEach var="row" items="${orgList}">
+		Name: ${row.companyName} <br/>
+		Year: ${row.yearOfIncoporation} <br/>
+		Zip: ${row.postalCode} <br/>
+		Emp Count: ${row.employeeCount} <br/>
+		Slogan: ${row.slogan} <br/><br/>
+	</c:forEach>
+</body>
+</html>
--- MVC-JNDI-Demo/src/main/java/com/spring/config/WebMvcConfig.java
+++ MVC-JNDI-Demo/src/main/java/com/spring/config/WebMvcConfig.java
@@ -1,8 +1,11 @@
 package com.spring.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.JstlView;
@@ -13,6 +16,16 @@ import org.springframework.web.servlet.view.UrlBasedViewResolver;
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
+	@Bean
+	public DataSource DataSource() {
+		final JndiDataSourceLookup dataSourceLookup = new JndiDataSourceLookup();
+		dataSourceLookup.setResourceRef(true);
+		
+		DataSource dataSource = dataSourceLookup.getDataSource("jdbc/springdb");
+		
+		return dataSource;
+	}
+	
 	@Bean
 	public UrlBasedViewResolver urlBasedViewResolver() {
 		 UrlBasedViewResolver resolver = new UrlBasedViewResolver();
