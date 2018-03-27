1af789cb78f4b69129e02dd807313b335875223e
David Jiang <djiang86@binghamton.edu>
2018-03-27 19:52:03 +0000

    JNDI datasource: created controller and service

---
+++ MVC-JNDI-Demo/src/main/java/com/spring/controller/OrganizationController.java
@@ -0,0 +1,32 @@
+package com.spring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.domain.Organization;
+import com.spring.service.OrganizationService;
+
+@Controller
+public class OrganizationController {
+
+	@Autowired
+	private OrganizationService organizationService;
+	
+	@RequestMapping
+	public String listOrganizationUsingSQLTag() {
+		
+		return "listOrganization1";
+	}
+	
+	@RequestMapping("/service")
+	public String listOrganizationUsingService(Model model) {
+		List<Organization> organizations = organizationService.getOrgList();
+		model.addAttribute("orgList", organizations);
+		
+		return "listOrganization2";
+	}
+}
